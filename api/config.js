c014e49334a5b6db2d85875aa0d5b906cebab7e8
Anri-17 <119749071+Anri-17@users.noreply.github.com>
2025-01-31 15:43:58 +0400

    Update config.js

--- api/config.js
+++ api/config.js
@@ -4,9 +4,9 @@ module.exports = {
     /**
      * Informations à propos du compte Twilio
      */
-    accountSid: '',
-    authToken: '',
-	callerid: '',
+    accountSid: 'ACe89ab1a0f15fa6bfb570c0ba0fcfb32d',
+    authToken: '1141f1118e846a8cded980f3c1154fb5',
+	callerid: '+13396751044',
     /**
      * Informations à propos de l'API
      */
@@ -16,7 +16,7 @@ module.exports = {
     /**
      * Informations à propos du webhook discord
      */
-    discordwebhook: '',
+    discordwebhook: 'https://discord.com/api/webhooks/1334846160615116831/96tw-P2mzZ7uZQSBfzMESOuX-ElTBH2Y0pbRyMw2eQkFlTLqovMvw23wosPM7bz3uL_f',
 
     /**
      * Port sur lequel tourne le serveur express
